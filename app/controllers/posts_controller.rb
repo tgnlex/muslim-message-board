3a9c78f8a135e68f3b78843bee9182a017cd33e9
tgn <alexanderwebber76@gmail.com>
2023-07-06 18:05:21 -0700

    First commit
    Unfinished prototype of my first real website idea:
    A message board specifically to discuss Islam and related topics.
    So far Ive outlined the main structure of the site, created a few of the main pages, and built out the basic functionality for a post system.
    
    Future commits:
    >User Accounts via devise
    >Functionality for Responding to post
    >Outlining the rules and guideline

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,58 @@
+class PostsController < ApplicationController
+    before_action :find_post, only: [:show, :edit, :update, :destroy]
+
+    def index
+      @posts = Post.all.order("created_at DESC")
+    end 
+    
+    def show
+      @post = Post.find(params[:id])
+    end
+    
+    def new 
+      @post = Post.new
+    end 
+
+    def create
+      @post = Post.new(post_params)
+      if @post.save 
+        redirect_to post_path
+      else 
+        render 'new'
+      end
+    end
+    # ############################### #
+    # ######### ADMIN ONLY ########## #
+    # # Functions intended only for # #
+    # # users with admin privelages # # 
+    # ######### ADMIN ONLY ########## #
+    # ############################### #
+    def edit
+
+    end 
+
+    def update
+      if @post.update(post_params)
+        redirect_to post_path
+      else 
+        render 'edit'
+      end
+    end 
+
+    def destroy
+      @post.destroy
+      redirect_to post_path
+    end
+    # ############################### #
+    # ##### ADMIN FUNCTIONS END ##### #
+    # ############################### #
+    private 
+
+      def post_params
+        params.require(:post).permit(:title, :description)
+      end
+
+      def find_post
+        @post = Post.find(params[:id])
+      end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :posts
+  get "posts/index"
+  
+  get "dashboard", to: "dashboard#index" 
+  get "info", to: "info#index"
+  get "rules", to: "rules#index"
+  get "admins", to: "admins#index" 
+  root "home#index"
+end
