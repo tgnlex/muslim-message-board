2d0ff75c3465b27df22bd5a2d3f0542626a006c5
tgn <alexanderwebber76@gmail.com>
2023-07-11 01:29:00 -0700

    user-to-post connection
    Setup connection between devise users model and posts model.
    Debugging for PostController functions still required

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_07_000509) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_11_063948) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
