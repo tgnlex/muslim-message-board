9a98b63ef35864a49bf5eb6557e7a0014c797aa1
tgn <alexanderwebber76@gmail.com>
2023-07-12 22:24:45 -0700

    Update routes.rb
    Configured comments route as a nested resource within posts route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :posts
+  resources :posts do 
+    resources :comments
+  end 
+  
   get "posts", to: "posts#index"
   get "dashboard", to: "dashboard#index" 
   get "info", to: "info#index"
