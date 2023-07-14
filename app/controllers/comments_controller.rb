7163b2fed20bfd3333da14999e12d1eb666656ed
tgn <alexanderwebber76@gmail.com>
2023-07-14 03:20:23 -0700

    comment form
    1. Created a _form partial in the comments-view.
    2. Rendered  the comment _form partial in show.html.erb so it will appear below each post page.
    3;  Implemented a create method in the comments controller.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
-end
+    def create
+      @post = Post.find(params[:message_id])
+      @comment = @message.comments.create(comment_params)
+      @comment.user_id = current_user_id;
+
+      if @comment.save
+        redirect_to post_path(@post)
+      else
+        render 'new'
+      end
+    end
+
+    private
+
+        def comment_params
+            params.require(:comment).permit(:content)
+end     end
