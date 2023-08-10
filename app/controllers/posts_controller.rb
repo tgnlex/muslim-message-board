0a29bea6c481c4d48e4c4056de7df11b5f74cd17
tgn <alexanderwebber76@gmail.com>
2023-08-10 04:44:02 -0700

    misc additions and fixes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
     def index
       @posts = Post.all.order("created_at DESC")
     end 
-    
+      
     def show
       @post = Post.find(params[:id])
     end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -4,5 +4,8 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do 
     get posts_url
     assert_response :success
+    assert_equal "index", @controller.action_name
+    assert_equal "application/x-www-form-urlencoded", @request.media_type
+    assert_match "Posts", @response.body
   end
 end
